27e4518803a39625a69470a4e1d7ea21d1dcda97
dinozindev <dinozindev@gmail.com>
2026-03-12 23:52:43 -0300

    feat: ajustando descrição do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="text-end p-6">developed by dinozindev.</div>
+    <div className="text-end p-6">Website developed by dinozindev.</div>
   )
 }
 
